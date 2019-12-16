36733e9ff4b14cae7b2b80a0b9b0c5a977a960a8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-16 04:17:46 +0000

    Deploy marunyak/js-band-final-task to github.com/marunyak/js-band-final-task.git:gh-pages

---
+++ precache-manifest.60b52eb7e67d58f8875eb32a512980c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b89904754a50a21284d682abb836e3b",
+    "url": "/js-band-final-task/index.html"
+  },
+  {
+    "revision": "277ff9d4f018447aa04c",
+    "url": "/js-band-final-task/static/css/main.7d4476ea.chunk.css"
+  },
+  {
+    "revision": "fce7c4f93e9f4693c1e9",
+    "url": "/js-band-final-task/static/js/2.c67d6f3c.chunk.js"
+  },
+  {
+    "revision": "277ff9d4f018447aa04c",
+    "url": "/js-band-final-task/static/js/main.51909cf3.chunk.js"
+  },
+  {
+    "revision": "a820abd2486a195c9b1e",
+    "url": "/js-band-final-task/static/js/runtime-main.d614273b.js"
+  },
+  {
+    "revision": "b1c4e6fe8bae09f7bb3815bff9ce6545",
+    "url": "/js-band-final-task/static/media/defaultPhoto.b1c4e6fe.jpg"
+  },
+  {
+    "revision": "b3066055daff49fac14dfc5700eac50c",
+    "url": "/js-band-final-task/static/media/shopping-cart.b3066055.png"
+  }
+]);
